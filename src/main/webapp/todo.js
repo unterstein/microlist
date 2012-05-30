7b70f94838d45dabac2b44d50106b3094dcf41c9
Johannes Unterstein <unterstein@me.com>
2012-05-30 07:33:14 +0200

    select the whole text is more usable

--- src/main/webapp/todo.js
+++ src/main/webapp/todo.js
@@ -11,13 +11,25 @@
         }
     };
 
+    $.fn.selectAll = function() {
+        if ($(this).get(0).setSelectionRange) {
+            $(this).get(0).setSelectionRange(0, pos);
+        } else if ($(this).get(0).createTextRange) {
+            var range = $(this).get(0).createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', 0);
+            range.select();
+        }
+    };
+
     $.fn.setLastCursorPosition = function() {
         $(this).setCursorPosition($(this).val().length);
     };
 })(jQuery);
 
 function handleCaretPositions() {
-    $(".nav input").setLastCursorPosition();
+    $(".nav input").selectAll();
 }
 
 $(function() {
